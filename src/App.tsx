f74132ab13955df06f28b74ff3ce38ebf5aca58d
MOGESH G <mogesh.dev@gmail.com>
2026-03-07 09:43:26 +0530

    Initial Setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="w-screen bg-red-200 h-screen justify-center text-center content-center">
+      Welcome
+    </div>
+  );
+};
+
+export default App;
+
